73c37dec42de1f342c3cd65e2a6b0ebf0cd5b7de
sanket20020312@gmail.com <sanket20020312@gmail.com>
2023-09-01 16:24:53 +0530

    Add Strings.java

---
+++ JAVA_STRINGS/Strings.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+public class Strings {
+    
+    public static void main(String[] args) {
+        String str ="abcd";
+        String str2 = new String("xyz");
+
+        Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter your first name : ");
+        // String name = sc.next(); // takes single word
+        // System.out.println(name);
+
+        // System.out.print("Enter your full name : ");
+        // String fullName = sc.nextLine();  // takes whole sentence
+        // System.out.println(fullName);
+        // System.out.print("Length of your name is :");
+        // System.out.println(fullName.length());
+
+        //Concatenation
+        String firstName = "Sanket";
+        String lastName = "Suryavanshi";
+        System.out.println((firstName+" "+lastName));
+
+    
+    }
+}
